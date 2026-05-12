1f5d83769da47d857925fc0090dbfe135319831b
Seif Eddine Marzougui <seifmarzougui555@gmail.com>
2026-05-12 13:11:53 +0100

    fixer card

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "sonner";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Revolution",
+  title: "MoveCar",
   description: "Service de convoyage de véhicules",
 };
 
